05544762d13b89fc37c861e73dfebad20703745e
jung0115 <jungim7490@gmail.com>
2024-05-22 19:10:19 +0900

    [조정미] 5주차 - 방문 길이 clear

---
+++ JeongMi/Week05_0523/Programmers05_2.js
@@ -0,0 +1,42 @@
+// 프로그래머스 - 방문 길기(Lv.2)
+function solution(dirs) {
+  let answer = 0;
+  let mapRow = Array.from(Array(11), () => Array(10).fill(false)); // 행
+  let mapCol = Array.from(Array(11), () => Array(10).fill(false)); // 열
+  
+  // 현재 위치 좌표
+  let curX = 5, curY = 5;
+  
+  for(let i = 0; i < dirs.length; i++) {
+    if(dirs.charAt(i) == 'U' && curY > 0) {
+      curY--;
+      if(!mapCol[curX][curY]) {
+        answer++;
+        mapCol[curX][curY] = true;
+      }
+    }
+    else if(dirs.charAt(i) == 'D' && curY < 10) {
+      if(!mapCol[curX][curY]) {
+        answer++;
+        mapCol[curX][curY] = true;
+      }
+      curY++;
+    }
+    else if(dirs.charAt(i) == 'R' && curX < 10) {
+      if(!mapRow[curY][curX]) {
+        answer++;
+        mapRow[curY][curX] = true;
+      }
+      curX++;
+    }
+    else if(dirs.charAt(i) == 'L' && curX > 0) {
+      curX--;
+      if(!mapRow[curY][curX]) {
+        answer++;
+        mapRow[curY][curX] = true;
+      }
+    }
+  }
+  
+  return answer;
+}
