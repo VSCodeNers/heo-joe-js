ec9c84885104faffbc0879c2fc62320d27d537c0
jung0115 <jungim7490@gmail.com>
2024-05-22 11:30:48 +0900

    [조정미] 5주차 - 호텔 대실 clear

--- JeongMi/Week05_0523/Programmers05_1.js
+++ JeongMi/Week05_0523/Programmers05_1.js
@@ -1,24 +1,21 @@
 // 프로그래머스 - 호텔 대실(Lv.2)
 function solution(book_time) {
   // 시간을 분 단위로 체크 24 * 60
-  let times = new Array(1440);
-  times.fill(0);
+  let times = Array(1440).fill(0);
   
   for(let i = 0; i < book_time.length; i++) {
-    let start = parseInt(book_time[i][0].substr(0, 2)) * 60; // 시 -> 분
-    start += parseInt(book_time[i][0].substr(3, 5)); // 분
-
-    let end = parseInt(book_time[i][1].substr(0, 2)) * 60; // 시 -> 분
-    end += parseInt(book_time[i][1].substr(3, 5)); // 분
+    let [startHour, startMinute] = book_time[i][0].split(':');
+    let start = parseInt(startHour) * 60 + parseInt(startMinute);
+    
+    let [endHour, endMinute] = book_time[i][1].split(':');
+    let end = parseInt(endHour) * 60 + parseInt(endMinute) + 10;
     
     // 해당 시간대에 방이 필요하면 증가
-    for(let j = start; j < end + 10 && j < 1440; j++) {
-        times[j]++;
+    for(let j = start; j < end && j < 1440; j++) {
+      times[j]++;
     }
   }
   
-  // 오름차순으로 정렬하면 맨 뒤에 제일 많이 필요한 방의 수가 나옴
-  times.sort();
-  
-  return times[1439];
+  // 제일 많이 필요한 방의 수
+  return Math.max(...times);
 }  
