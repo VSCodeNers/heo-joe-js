c6209339f8cffd25832c1a61301e8593fd717c21
jung0115 <jungim7490@gmail.com>
2024-06-20 22:32:04 +0900

    [README] 9주차 기간  변경

---
+++ JeongMi/Week09_0620/Baekjoon_1516.js
@@ -0,0 +1,13 @@
+// 백준 - 게임 개발(1516)
+const solution = ( input ) => {
+
+}
+
+/*
+테스트용: __dirname + '/input.txt'
+제출용: '/dev/stdin'
+*/
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+solution(input);
