33e8ba0042bac85ee524adc82d34628a69429e05
jung0115 <jungim7490@gmail.com>
2024-07-19 20:52:52 +0900

    [조정미] 12주차 - RGB거리 2(17404) 완료

--- JeongMi/Week12_0721/Beakjoon_17404.js
+++ JeongMi/Week12_0721/Beakjoon_17404.js
@@ -7,7 +7,36 @@ const solution = ( input ) => {
     house.push(input.shift().split(" ").map(el => parseInt(el)));
   }
 
+  let answer = Infinity;
   
+  for(let first = 0; first < 3; first++) {
+    let dp = [];
+
+    let one = first == 0 ? house[0][0] : Infinity;
+    let two = first == 1 ? house[0][1] : Infinity;
+    let three = first == 2 ? house[0][2] : Infinity;
+
+    // 첫번째 집
+    let firstList = [one, two, three];
+
+    dp.push(firstList);
+
+    for(let i = 1; i < N; i++) {
+      let line = [];
+      // 이전 집과 같은 경우를 제외
+      line.push(Math.min(dp[i-1][1], dp[i-1][2]) + house[i][0]);
+      line.push(Math.min(dp[i-1][0], dp[i-1][2]) + house[i][1]);
+      line.push(Math.min(dp[i-1][0], dp[i-1][1]) + house[i][2]);
+
+      dp.push(line);
+    }
+
+    for(let i = 0; i < 3; i++) {
+      if(first != i) answer = Math.min(dp[N - 1][i], answer);
+    }
+  }
+
+  console.log(answer);
 }
 /*
 테스트용: __dirname + '/input.txt'
